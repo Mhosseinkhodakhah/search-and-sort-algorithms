4e7cb2fb8db4a85423f477fa2135abcddf961e93
hossein <kianlucifer0098@gmail.com>
2024-12-14 21:38:04 +0330

    add merge sort

--- index.ts
+++ index.ts
@@ -103,6 +103,47 @@ class search {
     }
 
 
+    private async merge(left : number[], right :number[]) {
+        let resultArray = [],
+            leftIndex = 0,
+            rightIndex = 0;
+    
+        // Loop through both arrays, comparing elements and adding the smaller one to the resultArray
+        while (leftIndex < left.length && rightIndex < right.length) {
+            if (left[leftIndex] < right[rightIndex]) {
+                resultArray.push(left[leftIndex]);
+                leftIndex++; // Move to the next element in the `left` array
+            } else {
+                resultArray.push(right[rightIndex]);
+                rightIndex++; // Move to the next element in the `right` array
+            }
+        }
+    
+        // Concatenate the remaining elements from either `left` or `right` (if any)
+        return resultArray
+            .concat(left.slice(leftIndex))
+            .concat(right.slice(rightIndex));
+    }
+
+
+    async mergeSort(array : number[]):Promise<any> {
+        // Base case: If the array has only one element, return it (already sorted)
+        if (array.length === 1) {
+            return array;
+        }
+    
+        // Divide the array into two halves
+        const middle = Math.floor(array.length / 2); // Find the middle index
+        const left = array.slice(0, middle); // Split the array into left half
+        const right = array.slice(middle); // Split the array into right half
+    
+        // Recursively call mergeSort on the left and right halves
+        return this.merge(
+            await this.mergeSort(left), // Recursively sort the left half
+            await this.mergeSort(right) // Recursively sort the right half
+        );
+    }
+
 
 
 }
@@ -112,7 +153,7 @@ class search {
 const algorithms = new search()
 
 
-algorithms.insertionSort([9, 8, 5, 6, 3, 2, 1, 44, 5, 8, 99]).then((res) => {
+algorithms.mergeSort([9, 8, 5, 6, 3, 2, 1, 44, 5, 8, 99]).then((res) => {
     console.log( 'sorted is . . .', res)
 }).catch((err) => {
     console.log(err)
